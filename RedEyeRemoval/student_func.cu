249e0b9ea3075f301f9fdc5e7878a2886b719cff
JP <juhapekka.arimaa@gmail.com>
2016-08-28 10:01:42 +0300

    Finished implementing radix sort

--- RedEyeRemoval/student_func.cu
+++ RedEyeRemoval/student_func.cu
@@ -2,6 +2,7 @@
 //Radix Sorting
 
 #include "utils.h"
+#include <assert.h>
 #include <thrust/host_vector.h>
 
 /* Red Eye Removal
@@ -27,67 +28,64 @@
    0s so we have to know how many 0s there are to be able to start moving 1s
    into the correct position.
 
-   1) Histogram of the number of occurrences of each digit
-   2) Exclusive Prefix Sum of Histogram
-   3) Determine relative offset of each digit
-        For example [0 0 1 1 0 0 1]
-                ->  [0 1 0 1 2 3 2]
-   4) Combine the results of steps 2 & 3 to determine the final
-      output location for each element and move it there
-
    LSB Radix sort is an out-of-place sort and you will need to ping-pong values
    between the input and output buffers we have provided.  Make sure the final
-   sorted results end up in the output buffer!  Hint: You may need to do a copy
-   at the end.
+   sorted results end up in the output buffer!
 
- */
-
-__global__
-void predicate(unsigned int* const d_inputVals, int* d_predicate, int bit, size_t numElems)
-{
-    int id = threadIdx.x + blockDim.x * blockIdx.x;
-    if (id >= numElems) {
-        return;
-    }
-    
-    d_predicate[id] = d_inputVals[id] & bit == 0 ? 1 : 0;
-}
+*/
 
 __global__
-void scan(int* d_predicate, int* d_scan, int numElems)
+void scan(unsigned int* const d_inputVals, 
+          unsigned int* const d_scan, 
+          unsigned int bit, 
+          unsigned int block,
+          size_t numElems)
 {
-    int id = threadIdx.x + blockDim.x * blockIdx.x;
+    unsigned int offset = blockDim.x * block;
+    int id = threadIdx.x + offset;
     if (id >= numElems) {
         return;
     }
 
-    d_scan[id] = d_predicate[id];
+    d_scan[id] = (d_inputVals[id] & bit) == 0 ? 1 : 0;
     __syncthreads();
 
     // Inclusive Hillis-Steele scan
     unsigned int value = 0;
-	for (int i = 1; i < numElems; i <<= 1) {
-        value = id >= i ? d_scan[id-i] + d_scan[id] : d_scan[id];
+	for (int i = 1; i < blockDim.x; i <<= 1) {
+        value = id >= i+offset ? d_scan[id-i] + d_scan[id] : d_scan[id];
 		__syncthreads();
 		d_scan[id] = value;
 		__syncthreads();
-	}
+    }
+    if (offset > 0) {
+        d_scan[id] += d_scan[offset - 1];
+    }
 }
 
 __global__
 void move(unsigned int* const d_inputVals,
           unsigned int* const d_inputPos,
-          const size_t numElems,
-          int* d_predicate, 
-          int* d_scan)
+          unsigned int* const d_outputVals,
+          unsigned int* const d_outputPos,
+          unsigned int* const d_scan,
+          const size_t numElems,          
+          unsigned int bit)
 {
     int id = threadIdx.x + blockDim.x * blockIdx.x;
     if (id >= numElems) {
         return;
     }
-    int startPos = d_scan[numElems-1];
- 
-    // TODO
+    unsigned int startPos = d_scan[numElems-1]; 
+    unsigned int index = 0;
+    if ((d_inputVals[id] & bit) == 0) {
+        index = id == 0 ? 0 : d_scan[id-1];
+    } else {
+        index = startPos + id - (id == 0 ? 0 : d_scan[id-1]);
+    }
+    assert(index < numElems);
+    d_outputVals[index] = d_inputVals[id];
+    d_outputPos[index] = d_inputPos[id];
 }
 
 void your_sort(unsigned int* const d_inputVals,
@@ -96,20 +94,24 @@ void your_sort(unsigned int* const d_inputVals,
                unsigned int* const d_outputPos,
                const size_t numElems)
 {
-    const dim3 gridSize((numElems / 512) + 1);
-    const dim3 blockSize(512);
+    const dim3 gridSize((numElems / 1024) + 1);
+    const dim3 blockSize(1024);
 
-    int* d_predicate;
-    int* d_scan;
-    checkCudaErrors(cudaMalloc((void**) &d_predicate, sizeof(int) * numElems));
-    checkCudaErrors(cudaMalloc((void**) &d_scan, sizeof(int) * numElems));
+    unsigned int* d_scan;
+    checkCudaErrors(cudaMalloc(&d_scan, sizeof(unsigned int) * numElems));
 
-    unsigned int limit = std::numeric_limits<unsigned int>::max();
-    for (unsigned int bit = 1; bit < limit; bit <<= 1) {
-        predicate<<<gridSize, blockSize>>>(d_inputVals, d_predicate, bit, numElems);
-        scan<<<gridSize, blockSize>>>(d_predicate, d_scan, numElems);
+    for (unsigned int i = 0; i < 8 * sizeof(unsigned int); ++i) {
+        unsigned int bit = 1 << i;
+        for (unsigned int j = 0; j < gridSize.x; ++j) {
+            // Todo: these should be run in parallel and merged!
+            scan<<<1, blockSize>>>(d_inputVals, d_scan, bit, j, numElems);
+        }
+        move<<<gridSize, blockSize>>>(d_inputVals, d_inputPos, d_outputVals, d_outputPos, d_scan, numElems, bit);
+
+        checkCudaErrors(cudaMemcpy(d_inputVals, d_outputVals, numElems * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+        checkCudaErrors(cudaMemcpy(d_inputPos, d_outputPos, numElems * sizeof(unsigned int), cudaMemcpyDeviceToDevice));
+        checkCudaErrors(cudaGetLastError());
     }
-    
-    checkCudaErrors(cudaFree(d_predicate));
-    checkCudaErrors(cudaFree(d_scan));
+    cudaFree(d_scan);
+    checkCudaErrors(cudaGetLastError());
 }
