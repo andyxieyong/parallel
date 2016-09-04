38a557355059b612a29a0f7324b96dfae9343863
JP <juhapekka.arimaa@gmail.com>
2016-09-04 19:54:38 +0300

    Implemented image blending

--- ImageBlending/student_func.cu
+++ ImageBlending/student_func.cu
@@ -62,11 +62,8 @@
     In this assignment we will do 800 iterations.
    */
 
-
-
 #include "utils.h"
 #include <thrust/host_vector.h>
-#include <fstream>
 
 __device__
 bool isExterior(const uchar4* img, int pos)
@@ -76,8 +73,8 @@ bool isExterior(const uchar4* img, int pos)
 
 __global__
 void calculateMask(const uchar4* img,
-                   const int numRows, 
                    const int numCols,
+                   const int numRows,
                    int* mask)
 {
     int col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -108,61 +105,167 @@ void calculateMask(const uchar4* img,
     }
 }
 
-void your_blend(const uchar4* const h_sourceImg,  //IN
+__global__
+void separateChannels(const uchar4* img,
+                      float* red, float* green, float* blue,
+                      int numCols, int numRows)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (col >= numCols || row >= numRows) {
+        return;
+    }
+
+    int pos = numCols * row + col;
+    red[pos] = img[pos].x;
+    green[pos] = img[pos].y;
+    blue[pos] = img[pos].z;
+}
+
+__device__
+float getSum1Float(const float* dstImg, int* mask, float* prev, int pos)
+{
+    if (mask[pos] == 2) {
+        return prev[pos];
+    } else if (mask[pos] == 1) {
+        return dstImg[pos];
+    } else {
+        return 0.0f;
+    }
+}
+
+__global__
+void jacobi(const float* srcImg, const float* dstImg, int* mask,
+            float* prev, float* next,
+            int numCols, int numRows)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (col >= numCols || row >= numRows) {
+        return;
+    }
+
+    int pos = numCols * row + col;
+    if (mask[pos] != 2) {
+        next[pos] = dstImg[pos];
+        return;
+    }
+
+    int neighborUp = numCols * max(0, row-1) + col;
+    int neighborDown = numCols * min(numRows, row+1) + col;    
+    int neighborLeft = numCols * row + max(0, col-1);
+    int neighborRight = numCols * row + min(numCols, col+1);
+    
+    float sum1 = 0.0f;
+    sum1 += getSum1Float(dstImg, mask, prev, neighborUp);
+    sum1 += getSum1Float(dstImg, mask, prev, neighborDown);
+    sum1 += getSum1Float(dstImg, mask, prev, neighborLeft);
+    sum1 += getSum1Float(dstImg, mask, prev, neighborRight);
+    float sum2 = 0.0f;
+    sum2 += srcImg[pos] - srcImg[neighborUp];
+    sum2 += srcImg[pos] - srcImg[neighborDown];
+    sum2 += srcImg[pos] - srcImg[neighborLeft];
+    sum2 += srcImg[pos] - srcImg[neighborRight];
+    float newVal = (sum1 + sum2) / 4.0f;
+    next[pos] = min(255.0f, max(0.0f, newVal));
+}
+
+__global__
+void combineChannels(uchar4* out, float* red, float* green, float* blue, int numCols, int numRows)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (col >= numCols || row >= numRows) {
+        return;
+    }
+
+    int pos = numCols * row + col;
+    out[pos].x = red[pos];
+    out[pos].y = green[pos];
+    out[pos].z = blue[pos];
+}
+
+void your_blend(const uchar4* const h_sourceImg, //IN
                 const size_t numRowsSource, 
                 const size_t numColsSource,
                 const uchar4* const h_destImg, //IN
                 uchar4* const h_blendedImg) //OUT
 {
-    size_t numElems = numRowsSource * numColsSource;
+    int numElems = numRowsSource * numColsSource;
     const int block = 16;
     const dim3 blockSize(block, block, 1);
     const dim3 gridSize((numColsSource + block - 1) / block, 
                         (numRowsSource + block - 1) / block, 
                         1);
 
-    int* d_mask;
     uchar4* d_sourceImg;
-    checkCudaErrors(cudaMalloc(&d_mask, sizeof(int) * numElems));
     checkCudaErrors(cudaMalloc(&d_sourceImg, sizeof(uchar4) * numElems));
     checkCudaErrors(cudaMemcpy(d_sourceImg, h_sourceImg, sizeof(uchar4) * numElems, cudaMemcpyHostToDevice));
-    calculateMask<<<gridSize, blockSize>>>(d_sourceImg, numRowsSource, numColsSource, d_mask);
+    uchar4* d_destImg;
+    checkCudaErrors(cudaMalloc(&d_destImg, sizeof(uchar4) * numElems));
+    checkCudaErrors(cudaMemcpy(d_destImg, h_destImg, sizeof(uchar4) * numElems, cudaMemcpyHostToDevice));
+    
+    int* d_mask; 
+    checkCudaErrors(cudaMalloc(&d_mask, sizeof(int) * numElems));        
+    calculateMask<<<gridSize, blockSize>>>(d_sourceImg, numColsSource, numRowsSource, d_mask);
 
-  /* To Recap here are the steps you need to implement
-  
-     1) Compute a mask of the pixels from the source image to be copied
-        The pixels that shouldn't be copied are completely white, they
-        have R=255, G=255, B=255.  Any other pixels SHOULD be copied.
+    float* d_srcRed;
+    float* d_srcGreen;
+    float* d_srcBlue;
+    checkCudaErrors(cudaMalloc(&d_srcRed, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_srcGreen, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_srcBlue, sizeof(float) * numElems));
+    separateChannels<<<gridSize, blockSize>>>(d_sourceImg, d_srcRed, d_srcGreen, d_srcBlue, numColsSource, numRowsSource);
 
-     2) Compute the interior and border regions of the mask.  An interior
-        pixel has all 4 neighbors also inside the mask.  A border pixel is
-        in the mask itself, but has at least one neighbor that isn't.
+    float* d_dstRed;
+    float* d_dstGreen;
+    float* d_dstBlue;
+    checkCudaErrors(cudaMalloc(&d_dstRed, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_dstGreen, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_dstBlue, sizeof(float) * numElems));
+    separateChannels<<<gridSize, blockSize>>>(d_destImg, d_dstRed, d_dstGreen, d_dstBlue, numColsSource, numRowsSource);
 
-     3) Separate out the incoming image into three separate channels
+    float* d_nextRed;
+    float* d_nextGreen;
+    float* d_nextBlue;
+    float* d_prevRed;
+    float* d_prevGreen;
+    float* d_prevBlue;
+    checkCudaErrors(cudaMalloc(&d_nextRed, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_nextGreen, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_nextBlue, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_prevRed, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_prevGreen, sizeof(float) * numElems));
+    checkCudaErrors(cudaMalloc(&d_prevBlue, sizeof(float) * numElems));    
+    checkCudaErrors(cudaMemcpy(d_prevRed, d_srcRed, sizeof(float) * numElems, cudaMemcpyDeviceToDevice));
+    checkCudaErrors(cudaMemcpy(d_prevGreen, d_srcGreen, sizeof(float) * numElems, cudaMemcpyDeviceToDevice));
+    checkCudaErrors(cudaMemcpy(d_prevBlue, d_srcBlue, sizeof(float) * numElems, cudaMemcpyDeviceToDevice));
+    
+    for (int i = 0; i < 800; ++i) {
+        jacobi<<<gridSize, blockSize>>>(d_srcRed, d_dstRed, d_mask, d_prevRed, d_nextRed, numColsSource, numRowsSource);
+        jacobi<<<gridSize, blockSize>>>(d_srcGreen, d_dstGreen, d_mask, d_prevGreen, d_nextGreen, numColsSource, numRowsSource);
+        jacobi<<<gridSize, blockSize>>>(d_srcBlue, d_dstBlue, d_mask, d_prevBlue, d_nextBlue, numColsSource, numRowsSource);
+        checkCudaErrors(cudaMemcpy(d_prevRed, d_nextRed, sizeof(float) * numElems, cudaMemcpyDeviceToDevice));
+        checkCudaErrors(cudaMemcpy(d_prevGreen, d_nextGreen, sizeof(float) * numElems, cudaMemcpyDeviceToDevice));
+        checkCudaErrors(cudaMemcpy(d_prevBlue, d_nextBlue, sizeof(float) * numElems, cudaMemcpyDeviceToDevice));
+    }
 
-     4) Create two float(!) buffers for each color channel that will
-        act as our guesses.  Initialize them to the respective color
-        channel of the source image since that will act as our intial guess.
+    combineChannels<<<gridSize, blockSize>>>(d_sourceImg, d_nextRed, d_nextGreen, d_nextBlue, numColsSource, numRowsSource);
+    checkCudaErrors(cudaMemcpy(h_blendedImg, d_sourceImg, sizeof(uchar4) * numElems, cudaMemcpyDeviceToHost));
 
-     5) For each color channel perform the Jacobi iteration described 
-        above 800 times.
-
-     6) Create the output image by replacing all the interior pixels
-        in the destination image with the result of the Jacobi iterations.
-        Just cast the floating point values to unsigned chars since we have
-        already made sure to clamp them to the correct range.
-
-      Since this is final assignment we provide little boilerplate code to
-      help you.  Notice that all the input/output pointers are HOST pointers.
-
-      You will have to allocate all of your own GPU memory and perform your own
-      memcopies to get data in and out of the GPU memory.
-
-      Remember to wrap all of your calls with checkCudaErrors() to catch any
-      thing that might go wrong.  After each kernel call do:
-
-      cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-
-      to catch any errors that happened while executing the kernel.
-  */
+    checkCudaErrors(cudaFree(d_sourceImg));
+    checkCudaErrors(cudaFree(d_destImg));
+    checkCudaErrors(cudaFree(d_mask));
+    checkCudaErrors(cudaFree(d_srcRed));
+    checkCudaErrors(cudaFree(d_srcGreen));
+    checkCudaErrors(cudaFree(d_srcBlue));
+    checkCudaErrors(cudaFree(d_dstRed));
+    checkCudaErrors(cudaFree(d_dstGreen));
+    checkCudaErrors(cudaFree(d_dstBlue));
+    checkCudaErrors(cudaFree(d_nextRed));
+    checkCudaErrors(cudaFree(d_nextGreen));
+    checkCudaErrors(cudaFree(d_nextBlue));
+    checkCudaErrors(cudaFree(d_prevRed));
+    checkCudaErrors(cudaFree(d_prevGreen));
+    checkCudaErrors(cudaFree(d_prevBlue));
 }
